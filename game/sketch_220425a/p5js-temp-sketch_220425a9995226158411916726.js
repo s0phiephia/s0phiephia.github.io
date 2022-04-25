58ee694f3a8fd3115b0361b49b1a937dc6fdb1f3
s0phiephia <101031632+s0phiephia@users.noreply.github.com>
2022-04-25 16:58:00 -0700

    update

---
+++ game/sketch_220425a/p5js-temp-sketch_220425a9995226158411916726.js
@@ -0,0 +1,47 @@
+
+
+
+
+var ballx = 300;
+var bally = 300;
+var ballSize = 40;
+var score = 0;
+var img1, img2;
+
+
+function preload(){
+  img1 = loadImage('https://s0phiephia.github.io/game/triceratop.png');
+  img2 = loadImage('https://s0phiephia.github.io/game/mountains.jpg');
+}
+
+function setup() {
+  createCanvas(600,600);
+  textAlign(CENTER);
+  textSize(20);
+} // end of setup
+
+function draw() {
+  background(img2);
+  
+  levelOne();
+  text(("Score: " + score), width/2, 40);
+  
+} // end of draw
+
+function levelOne(){
+  text("level 1", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if(distToBall < ballSize/2){
+    ballx = random(width);
+    bally = random(height);
+    score = score + 1;
+  }
+  
+  
+  line(ballx+25, bally+25, mouseX, mouseY);
+  
+  image(img1, ballx, bally, ballSize+10, ballSize+10);
+  
+  //ellipse(ballx, bally, ballSize, ballSize);
+  
+} // end of level 1

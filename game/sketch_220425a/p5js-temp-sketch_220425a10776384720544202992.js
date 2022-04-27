c90dad060891d031dccbe89b7d007c8aa45154a0
s0phiephia <101031632+s0phiephia@users.noreply.github.com>
2022-04-27 15:50:40 -0700

    update

--- game/sketch_220425a/p5js-temp-sketch_220425a10776384720544202992.js
+++ game/sketch_220425a/p5js-temp-sketch_220425a10776384720544202992.js
@@ -7,7 +7,7 @@ var bally = 300;
 var ballSize = 40;
 var score = 0;
 var img1, img2;
-
+var gameState = "L1" ;
 
 function preload(){
   img1 = loadImage('https://s0phiephia.github.io/game/triceratop.png');
@@ -21,10 +21,19 @@ function setup() {
 } // end of setup
 
 function draw() {
+ 
+  if(gameState == "L1"){
   background(img2);
-  
   levelOne();
+  }
+  
+  if(gameState == "L2"){
+    background(125);
+    levelTwo();
+  }
+  
   text(("Score: " + score), width/2, 40);
+  
   
 } // end of draw
 
@@ -36,12 +45,26 @@ function levelOne(){
     bally = random(height);
     score = score + 1;
   }
-  
-  
-  line(ballx+25, bally+25, mouseX, mouseY);
-  
-  image(img1, ballx, bally, ballSize+10, ballSize+10);
-  
+  if(score>= 5){
+    gameState = "L2";
+  }
+  image(img1, ballx, bally, ballSize, ballSize);
+  line(ballx + 20 , bally + 20 , mouseX, mouseY);
   //ellipse(ballx, bally, ballSize, ballSize);
   
 } // end of level 1
+
+
+function levelTwo(){
+  text("level 2", width/2, height-20);
+  var distToBall = dist(ballx, bally, mouseX, mouseY);
+  if(distToBall < ballSize/2){
+    ballx = random(width);
+    bally = random(height);
+    score = score + 1;
+  }
+  line(ballx + 20 , bally + 20 , mouseX, mouseY);
+  image(img1, ballx, bally, ballSize, ballSize);
+
+  
+} //end level 2
